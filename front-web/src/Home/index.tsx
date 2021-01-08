0ab7fea45908ef14768bd3363534179c9d88bcf5
GuzzMega <guzzt.mega@gmail.com>
2021-01-07 23:02:24 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,27 +1,30 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
-
+import Footer from '../Footer/index';
 function Home(){
     
     return (
-        <div className="home-container">
-            <div className="home-content">
-                <div className="home-actions">
-                    <h1 className="home-title">
-                        Faça seu pedido <br/> que entregamos <br/> para você!
-                    </h1>
-                    <h3 className="hom-subtitle">
-                        Escolha sua refeiçao e em alguns instantes <br/> levaremos na sua porta.
-                    </h3>
-                    <a href="orders" className="home-btn-order">
-                        FAZER PEDIDO!
-                    </a>
-                </div>
-                <div className="home-image">
-                    <MainImage />
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <h1 className="home-title">
+                            Faça seu pedido <br/> que entregamos <br/> para você!
+                        </h1>
+                        <h3 className="hom-subtitle">
+                            Escolha sua refeiçao e em alguns instantes <br/> levaremos na sua porta.
+                        </h3>
+                        <a href="orders" className="home-btn-order">
+                            FAZER PEDIDO!
+                        </a>
+                    </div>
+                    <div className="home-image">
+                        <MainImage />
+                    </div>
                 </div>
             </div>
-        </div>
+            <Footer/>
+        </>
     )
 }
 
